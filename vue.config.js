8b48287cf9aeba464abceafd0432c11b708d71a8
abelit <ychenid@live.com>
2022-02-07 17:41:10 +0800

    add vue config

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  outputDir: "dist",
+  assetsDir: "static",
+  indexPath: "index.html",
+  devServer: {
+    port: 8080,
+    open: true,
+  },
+};
